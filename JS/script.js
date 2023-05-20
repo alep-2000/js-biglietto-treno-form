6d0b04ecf8d327346d4a29c4d9c20ac622e176da
alep-2000 <alessiopiras056@gmail.com>
2023-05-20 18:33:52 +0200

    [FEAT STYLE]Complete exercise

--- JS/script.js
+++ JS/script.js
@@ -2,23 +2,39 @@ let button= document.getElementById('button');
 
 button.addEventListener('click', function(){
     let km= parseInt(document.getElementById('km').value);
-    console.log(km)
     let age= parseInt(document.getElementById('age').value);
-    console.log(age)
+    let name= document.getElementById('name').value;
     let prizetokm= 0.21;
     let prizefinal= prizetokm * km;
     if(!isNaN(km)){
         if (age < 18){
-            prizefinal *= 0.8.toFixed(2);
+            prizefinal *= 0.8;
             console.log(prizefinal);
         }else if(age > 65){
-            prizefinal *=0.6.toFixed(2);
+            prizefinal *=0.6;
             console.log(prizefinal);
         }else{
-            prizefinal.toFixed(2);
+            prizefinal;
             console.log(prizefinal);
         } 
+
+    document.getElementById('show-name').innerText =name;
+    document.getElementById('show-km').innerText =km;
+    document.getElementById('show-age').innerText =age;
+    document.getElementById('show-price').innerText = prizefinal.toFixed(2) +'€';
     }
+
+})
+
+
+document.getElementById('reset').addEventListener('click', function(){
+    document.getElementById('name').value='';
+    document.getElementById('km').value='';
+    document.getElementById('age').value='';
+    document.getElementById('show-name').innerText ='';
+    document.getElementById('show-km').innerText ='';
+    document.getElementById('show-age').innerText ='';
+    document.getElementById('show-price').innerText = '';
 })
 
 
