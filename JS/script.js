868d3e8b7bfd1c2ad0e3b76a3376a89f5e415957
alep-2000 <alessiopiras056@gmail.com>
2023-05-20 18:02:49 +0200

    [FEAT STYLE]Add structure to the ticket of  train

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,30 @@
-let km = prompt ('Inserisci quanti km vuoi percorrere')
-console.log(km)
+let button= document.getElementById('button');
 
-let age = prompt ('Inserisci la tua età')
-console.log(age)
+button.addEventListener('click', function(){
+    let km= parseInt(document.getElementById('km').value);
+    console.log(km)
+    let age= parseInt(document.getElementById('age').value);
+    console.log(age)
+    let prizetokm= 0.21;
+    let prizefinal= prizetokm * km;
+    if(!isNaN(km)){
+        if (age < 18){
+            prizefinal *= 0.8.toFixed(2);
+            console.log(prizefinal);
+        }else if(age > 65){
+            prizefinal *=0.6.toFixed(2);
+            console.log(prizefinal);
+        }else{
+            prizefinal.toFixed(2);
+            console.log(prizefinal);
+        } 
+    }
+})
 
-let prize = (0.21 * km)
-console.log(prize)
 
-prizefinal= prize
 
-if (age < 18){
-    prizefinal= (prize * 20 / 100)
-    console.log(prizefinal)
-}else if (age > 65){
-    prizefinal= (prize * 40 / 100)
-    console.log(prizefinal)
-}else{
-    prizefinal = prize
-}
+
+
+
+
+
